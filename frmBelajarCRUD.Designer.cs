db532c8ef6ebcf8a3b1085f491eb3e6900556fa6
Haykalreka <haikalreka@gmail.com>
2024-02-28 15:33:17 +0700

    Add project files.

---
+++ frmBelajarCRUD.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace belajarCRUD
+{
+    partial class frmBelajarCRUD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            Materi = new Label();
+            dgvCRUD = new DataGridView();
+            pbGambar = new PictureBox();
+            btnHapus = new Button();
+            btnUbah = new Button();
+            btnSimpan = new Button();
+            btnCari = new Button();
+            lbl_namabarang = new Label();
+            txtDeskripsi = new TextBox();
+            txtNamabarang = new TextBox();
+            lbl_deskripsi = new Label();
+            txtId = new TextBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvCRUD).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbGambar).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Orange;
+            panel1.Controls.Add(Materi);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 59);
+            panel1.TabIndex = 12;
+            // 
+            // Materi
+            // 
+            Materi.AutoSize = true;
+            Materi.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            Materi.ForeColor = Color.Black;
+            Materi.Location = new Point(15, 12);
+            Materi.Name = "Materi";
+            Materi.Size = new Size(443, 30);
+            Materi.TabIndex = 0;
+            Materi.Text = "Belajar CRUD (Create, Read, Update, Delete)\r\n";
+            // 
+            // dgvCRUD
+            // 
+            dgvCRUD.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCRUD.Location = new Point(443, 97);
+            dgvCRUD.Name = "dgvCRUD";
+            dgvCRUD.RowTemplate.Height = 25;
+            dgvCRUD.Size = new Size(302, 336);
+            dgvCRUD.TabIndex = 22;
+            dgvCRUD.CellContentClick += dgvCRUD_CellContentClick;
+            // 
+            // pbGambar
+            // 
+            pbGambar.BackColor = SystemColors.ControlDark;
+            pbGambar.Location = new Point(136, 155);
+            pbGambar.Name = "pbGambar";
+            pbGambar.Size = new Size(290, 219);
+            pbGambar.TabIndex = 21;
+            pbGambar.TabStop = false;
+            // 
+            // btnHapus
+            // 
+            btnHapus.Location = new Point(370, 409);
+            btnHapus.Name = "btnHapus";
+            btnHapus.Size = new Size(56, 23);
+            btnHapus.TabIndex = 20;
+            btnHapus.Text = "Hapus";
+            btnHapus.UseVisualStyleBackColor = true;
+            btnHapus.Click += btnHapus_Click;
+            // 
+            // btnUbah
+            // 
+            btnUbah.Location = new Point(308, 409);
+            btnUbah.Name = "btnUbah";
+            btnUbah.Size = new Size(56, 23);
+            btnUbah.TabIndex = 19;
+            btnUbah.Text = "Ubah";
+            btnUbah.UseVisualStyleBackColor = true;
+            btnUbah.Click += btnUbah_Click;
+            // 
+            // btnSimpan
+            // 
+            btnSimpan.Location = new Point(246, 409);
+            btnSimpan.Name = "btnSimpan";
+            btnSimpan.Size = new Size(56, 23);
+            btnSimpan.TabIndex = 18;
+            btnSimpan.Text = "Simpan";
+            btnSimpan.UseVisualStyleBackColor = true;
+            btnSimpan.Click += btnSimpan_Click;
+            // 
+            // btnCari
+            // 
+            btnCari.Location = new Point(335, 380);
+            btnCari.Name = "btnCari";
+            btnCari.Size = new Size(91, 23);
+            btnCari.TabIndex = 17;
+            btnCari.Text = "Pilih Gambar";
+            btnCari.UseVisualStyleBackColor = true;
+            btnCari.Click += btnCari_Click;
+            // 
+            // lbl_namabarang
+            // 
+            lbl_namabarang.AutoSize = true;
+            lbl_namabarang.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbl_namabarang.Location = new Point(18, 97);
+            lbl_namabarang.Name = "lbl_namabarang";
+            lbl_namabarang.Size = new Size(100, 20);
+            lbl_namabarang.TabIndex = 16;
+            lbl_namabarang.Text = "Nama Barang\r\n";
+            // 
+            // txtDeskripsi
+            // 
+            txtDeskripsi.Location = new Point(136, 126);
+            txtDeskripsi.Name = "txtDeskripsi";
+            txtDeskripsi.Size = new Size(290, 23);
+            txtDeskripsi.TabIndex = 15;
+            // 
+            // txtNamabarang
+            // 
+            txtNamabarang.Location = new Point(136, 97);
+            txtNamabarang.Name = "txtNamabarang";
+            txtNamabarang.Size = new Size(290, 23);
+            txtNamabarang.TabIndex = 14;
+            // 
+            // lbl_deskripsi
+            // 
+            lbl_deskripsi.AutoSize = true;
+            lbl_deskripsi.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbl_deskripsi.Location = new Point(18, 126);
+            lbl_deskripsi.Name = "lbl_deskripsi";
+            lbl_deskripsi.Size = new Size(69, 20);
+            lbl_deskripsi.TabIndex = 13;
+            lbl_deskripsi.Text = "Deskripsi";
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(18, 166);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(49, 23);
+            txtId.TabIndex = 23;
+            // 
+            // frmBelajarCRUD
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtId);
+            Controls.Add(panel1);
+            Controls.Add(dgvCRUD);
+            Controls.Add(pbGambar);
+            Controls.Add(btnHapus);
+            Controls.Add(btnUbah);
+            Controls.Add(btnSimpan);
+            Controls.Add(btnCari);
+            Controls.Add(lbl_namabarang);
+            Controls.Add(txtDeskripsi);
+            Controls.Add(txtNamabarang);
+            Controls.Add(lbl_deskripsi);
+            Name = "frmBelajarCRUD";
+            Text = "frmBelajarCRUD";
+            Load += FrmBelajarCRUD_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvCRUD).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbGambar).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label Materi;
+        private DataGridView dgvCRUD;
+        private PictureBox pbGambar;
+        private Button btnHapus;
+        private Button btnUbah;
+        private Button btnSimpan;
+        private Button btnCari;
+        private Label lbl_namabarang;
+        private TextBox txtDeskripsi;
+        private TextBox txtNamabarang;
+        private Label lbl_deskripsi;
+        private TextBox txtId;
+    }
+}
---
+++ frmBelajarCRUD.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace belajarCRUD
+{
+    public partial class frmBelajarCRUD : Form
+    {
+        public frmBelajarCRUD()
+        {
+            InitializeComponent();
+        }
+
+        MySqlConnection conn = new MySqlConnection
+            ("datasource=localhost;port=3306;" + "Initial Catalog='db_crud';username=root;password=");
+
+        public void showData()
+        {
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM tbl_crud", conn);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dgvCRUD.RowTemplate.Height = 150;
+            dgvCRUD.AllowUserToAddRows = false;
+            dgvCRUD.DataSource = table;
+            DataGridViewImageColumn imgcol = new DataGridViewImageColumn();
+            imgcol = (DataGridViewImageColumn)dgvCRUD.Columns[3];
+            imgcol.ImageLayout = DataGridViewImageCellLayout.Stretch;
+            dgvCRUD.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void btnCari_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+
+            opf.Filter = "Choose Image (*.JPG;*.PNG;*.GIF) | *.jpg;*.png;*.gif";
+
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                pbGambar.Image = Image.FromFile(opf.FileName);
+            }
+        }
+
+        private void dgvCRUD_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Byte[] img = (Byte[])dgvCRUD.CurrentRow.Cells[3].Value;
+
+            MemoryStream ms = new MemoryStream(img);
+
+            pbGambar.Image = Image.FromStream(ms);
+
+            txtId.Text = dgvCRUD.CurrentRow.Cells[0].Value.ToString();
+            txtNamabarang.Text = dgvCRUD.CurrentRow.Cells[1].Value.ToString();
+            txtDeskripsi.Text = dgvCRUD.CurrentRow.Cells[2].Value.ToString();
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            MemoryStream ms = new MemoryStream();
+
+            pbGambar.Image.Save(ms, pbGambar.Image.RawFormat);
+
+            byte[] img = ms.ToArray();
+
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO tbl_crud " + "(nama, deskripsi, gambar) VALUES (@nama, @deskripsi, @image)", conn);
+
+            cmd.Parameters.Add("@nama", MySqlDbType.VarChar).Value = txtNamabarang.Text;
+            cmd.Parameters.Add("@deskripsi", MySqlDbType.VarChar).Value = txtDeskripsi.Text;
+            cmd.Parameters.Add("@image", MySqlDbType.Blob).Value = img;
+
+            ExecMyQuery(cmd, "Data Inserted");
+        }
+
+        public void ExecMyQuery(MySqlCommand mcomd, string myMsg)
+        {
+            conn.Open();
+            if (mcomd.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show(myMsg);
+            }
+            else
+            {
+                MessageBox.Show("Query Not Executed");
+            }
+            conn.Close();
+        }
+
+        private void FrmBelajarCRUD_Load(object sender, EventArgs e)
+        {
+            showData();
+        }
+
+        private void btnUbah_Click(object sender, EventArgs e)
+        {
+            MemoryStream ms = new MemoryStream();
+
+            pbGambar.Image.Save(ms, pbGambar.Image.RawFormat);
+
+            byte[] img = ms.ToArray();
+
+            MySqlCommand cmd = new MySqlCommand("UPDATE tbl_crud SET nama=@nama, " + "deskripsi=@deskripsi, gambar=@image WHERE id=@id", conn);
+
+            cmd.Parameters.Add("@id", MySqlDbType.VarChar).Value = txtId.Text;
+            cmd.Parameters.Add("@nama", MySqlDbType.VarChar).Value = txtNamabarang.Text;
+            cmd.Parameters.Add("@deskripsi", MySqlDbType.VarChar).Value = txtDeskripsi.Text;
+            cmd.Parameters.Add("@image", MySqlDbType.Blob).Value = img;
+
+            ExecMyQuery(cmd, "Data Updated");
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            MySqlCommand cmd = new MySqlCommand("DELETE FROM tbl_crud WHERE id=@id", conn);
+            cmd.Parameters.Add("@id", MySqlDbType.VarChar).Value = txtId.Text;
+
+            ExecMyQuery(cmd, "Data Deleted");
+        }
+    }
+}
